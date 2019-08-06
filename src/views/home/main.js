e42bfc50328c754e26ef7aceec361a8e0a254eef
CassianaSL <cassianasilveira64@gmail.com>
2019-08-06 20:52:57 -0300

    slider

--- src/views/home/main.js
+++ src/views/home/main.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.carousel');
+    const instances = M.Carousel.init(elems);
 });
 
 var instance = M.Carousel.init({
     fullWidth: true,
     indicators: true
 });
+
